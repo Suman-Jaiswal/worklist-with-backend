d74fea730faae600653cd55382601c61ee65d563
Suman Jaiswal <sumanj631@gmail.com>
2021-09-07 21:55:32 +0530

    final

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 const path = require('path')
 const planRouter = require('./routes/api/plans')
@@ -19,11 +20,7 @@ mongoose.connect(DB_URI, {
 
 
 //middleware
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://ninja-worklist.herokuapp.com/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors)
 
 app.use(express.json())
 app.use('/api/plans', planRouter)
