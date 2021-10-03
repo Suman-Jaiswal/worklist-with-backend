b0c7f236cd8fb53d2c22269842a2bf58037c7198
Suman Jaiswal <sumanj631@gmail.com>
2021-10-04 00:03:23 +0530

    .

--- client/src/components/AddTopicBtn.jsx
+++ client/src/components/AddTopicBtn.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
 import { PlanContext } from '../contexts/PlanContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,7 +9,8 @@ export default function AddTopicBtn({ variant, color, planID }) {
 
     const { dispatch } = useContext(PlanContext)
     const [open, setOpen] = useState(false)
-    const [title, setTitle] = useState('')
+    const [input, setInput] = useState('')
+    const [topics, setTopics] = useState([])
 
     const openModal = () => {
         setOpen(true)
@@ -17,28 +18,40 @@ export default function AddTopicBtn({ variant, color, planID }) {
     const closeModal = () => {
         setOpen(false)
     }
+    useEffect(() => {
+
+        const raw = input.split('\n')
+        const array = [...raw]
+        const rawTopics = []
+
+        for (let i = 0; i < array.length; i++) {
+            const obj = { title: array[i], planID }
+            rawTopics.push(obj)
+        }
+
+        setTopics(rawTopics)
+
+    }, [input, planID])
 
     const handleSubmit = (e) => {
         e.preventDefault()
         closeModal()
         axios.post(`/api/topics/${planID}`, {
-            topics: [{
-                title,
-                planID
-            }],
+            topics
         })
         .then(res => {
             dispatch({ type: 'ADD_TOPICS', payload: res.data })
         })
         .catch(err => console.log(err))
 
-        setTitle('')
+        setInput('')
+        setTopics([])
     }
 
     return (
         <>
             <Button variant={variant} className={color} onClick={openModal}  size='sm'  aria-labelledby="contained-modal-title-vcenter" >
-                <FontAwesomeIcon icon={faPlus} size='sm' /> <span className='ms-1 create-text'>Add Topic</span>
+                <FontAwesomeIcon icon={faPlus} size='sm' /> <span className='ms-1 create-text'>Add Topics</span>
             </Button>
 
             <Modal show={open} onHide={closeModal} >
@@ -47,13 +60,14 @@ export default function AddTopicBtn({ variant, color, planID }) {
 
                     <Modal.Header className='text-center' closeButton >
                         <div className='m-auto bold display-6' >
-                            Add Topic
+                            Add Topics
                         </div>
                     </Modal.Header>
 
                     <Modal.Body>
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control className='mb-4' onChange={(e) => setTitle(e.target.value)} placeholder={'Title'} />
+                        <Form.Label>Titles</Form.Label>
+                        <Form.Control className='mb-4' as='textarea' rows={8} onChange={(e) => setInput(e.target.value)}
+                            placeholder={'Add topics in each line \n ********example*******\n Topic 1\n Topic 2\n Topic 3\n  ...\n '} />
                     </Modal.Body>
 
                     <Modal.Footer>
